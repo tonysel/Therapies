893cc53742ff3e79eb368a459b98a1a6008bbf66
Tony Sellitto <tonysellitto@MBP-di-Tony.homenet.telecomitalia.it>
2018-09-25 22:38:51 +0200

    first commit

---
+++ NotiExtension/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  NotiExtension
+//
+//  Created by Tony Sellitto on 25/07/18.
+//  Copyright © 2018 TonySellitto. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
